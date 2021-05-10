37a53b0b9992adac6a9378e1d53ccfd3c2445a55
wangxi3 <wangxi3@huya.com>
2021-05-10 14:56:22 +0800

    fix component scan miss

--- src/main/java/net/prominic/groovyls/webSocket/WebSocketConfig.java
+++ src/main/java/net/prominic/groovyls/webSocket/WebSocketConfig.java
@@ -2,15 +2,21 @@ package net.prominic.groovyls.webSocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 import org.springframework.web.socket.server.standard.ServerEndpointRegistration;
 
 @Configuration
+@EnableWebSocket
 public class WebSocketConfig {
 	
 	@Bean
 	public ServerEndpointRegistration serverEndpointRegistration(){
 		return new ServerEndpointRegistration("/groovy",GroovyLspEndpoint.class);
 	}
-	
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
 	
 }
